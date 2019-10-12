d4f8520a220e36ba2750e46213c44f9e8c89318f
kristie <kristiemconnor@gmail.com>
2019-10-12 00:19:52 -0400

    adds third user, adds more ingredients. fascinating pkey db error.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(id: 1, first_name: "Bud", last_name: "Abbot", email: "bud@mail.com")
-User.create(id: 2, first_name: "Shanda", last_name: "Poyner", email: "shanda@mail.com")
+User.create(id: 03, first_name: "Bud", last_name: "Abbot", email: "bud@mail.com")
+User.create(id: 04, first_name: "Shanda", last_name: "Poyner", email: "shanda@mail.com")
 
-Ingredient.create(id: 1, name: "bananas", expiration: (2019-12-2), user_id: 1)
-Ingredient.create(id: 2, name: "cherries", expiration: (2019-12-1), user_id: 1)
-Ingredient.create(id: 3, name: "flour", expiration: (2020-2-1), user_id: 1)
-Ingredient.create(id: 4, name: "cream cheese", expiration: (2019-12-1), user_id: 2)
-Ingredient.create(id: 5, name: "plain yogurt", expiration: (2019-12-10), user_id: 2)
-Ingredient.create(id: 6, name: "penne", expiration: (2020-1-1), user_id: 2)
-Ingredient.create(id: 7, name: "canned corn", expiration: (2020-2-2), user_id: 2)
+Ingredient.create(id: 1, name: "bananas", expiration: "%12 %01 %2019", user_id: 1)
+Ingredient.create(id: 2, name: "cherries", expiration: "%12 %02 %2019", user_id: 1)
+Ingredient.create(id: 3, name: "flour", expiration: "%03 %22 %2020",  user_id: 1)
+Ingredient.create(id: 4, name: "cream cheese", expiration: "%11 %28 %2019", user_id: 2)
+Ingredient.create(id: 5, name: "plain yogurt", expiration: "%12 % 1 %2019", user_id: 2)
+Ingredient.create(id: 6, name: "penne", expiration: "%01 %29 %2020", 
+ user_id: 2)
+Ingredient.create(id: 7, name: "canned corn", expiration: "%01 %10 %2020", user_id: 2)
