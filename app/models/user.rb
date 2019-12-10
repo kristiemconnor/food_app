fee38f63672d07d82cacb685a6b518a93b97e8ab
kristie <kristiemconnor@gmail.com>
2019-12-09 20:00:08 -0500

    updates email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 
   has_many :ingredients
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
 
 end
